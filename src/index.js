7eb72add167d7e451938efcaea6e09fc5e45d28c
sidiousvic <victor6rene@gmail.com>
2019-07-04 22:09:02 +0900

    Simplified UI, prepared for further three.js/react development

--- src/index.js
+++ src/index.js
@@ -1,25 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import "./css/index.css";
+import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
-
-// animate subtitle
-const title = document.querySelector("#title-description");
-(function switchTitles() {
-  const descriptions = [
-    "DARKPSYCHNECROMETALIST",
-    "MARTIANECROPHAGIST",
-    "ULTRAVENOMIST",
-    "NECROMANTULIST",
-    "OPHIOPHAGIST"
-  ];
-  let random = Math.floor(Math.random() * 3) + 1;
-  window.setInterval(function() {
-    while (title.innerHTML === descriptions[random]) {
-      random = Math.floor(Math.random() * 5);
-    }
-    title.innerHTML = descriptions[random];
-  }, 3000);
-})();
