5068b53a32e92c5887b3df9a946373da45d28203
sidiousvic <victor6rene@gmail.com>
2020-01-16 21:54:24 +0900

    experiment

---
+++ .cache/plugins/strapi-plugin-content-type-builder/admin/src/containers/App/index.js
@@ -0,0 +1,40 @@
+/**
+ *
+ * App
+ *
+ */
+
+import React, { Suspense, lazy } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import { LoadingIndicatorPage } from 'strapi-helper-plugin';
+import pluginId from '../../pluginId';
+import DataManagerProvider from '../DataManagerProvider';
+import icons from './utils/icons.json';
+import Wrapper from './Wrapper';
+
+const ListView = lazy(() => import('../ListView'));
+
+import RecursivePath from '../RecursivePath';
+
+const App = () => {
+  return (
+    <Wrapper>
+      <DataManagerProvider allIcons={icons}>
+        <Suspense fallback={<LoadingIndicatorPage />}>
+          <Switch>
+            <Route
+              path={`/plugins/${pluginId}/content-types/:uid`}
+              component={ListView}
+            />
+            <Route
+              path={`/plugins/${pluginId}/component-categories/:categoryUid`}
+              component={RecursivePath}
+            />
+          </Switch>
+        </Suspense>
+      </DataManagerProvider>
+    </Wrapper>
+  );
+};
+
+export default App;
