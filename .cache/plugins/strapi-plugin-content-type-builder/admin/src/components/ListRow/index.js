2b8c363eab2b75df2ec14c7df0505015044f475c
sidiousvic <victor6rene@gmail.com>
2020-01-16 22:01:55 +0900

    fixed

---
+++ .cache/plugins/strapi-plugin-content-type-builder/admin/src/components/ListRow/index.js
@@ -0,0 +1,209 @@
+import React, { memo } from 'react';
+import PropTypes from 'prop-types';
+import { get } from 'lodash';
+import { FormattedMessage } from 'react-intl';
+import { AttributeIcon } from '@buffetjs/core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import pluginId from '../../pluginId';
+import useDataManager from '../../hooks/useDataManager';
+import getTrad from '../../utils/getTrad';
+import Curve from '../../icons/Curve';
+import UpperFist from '../UpperFirst';
+import Wrapper from './Wrapper';
+
+function ListRow({
+  configurable,
+  name,
+  nature,
+  onClick,
+  plugin,
+  target,
+  targetUid,
+  type,
+  mainTypeName,
+  editTarget,
+  firstLoopComponentName,
+  firstLoopComponentUid,
+  isFromDynamicZone,
+  repeatable,
+  secondLoopComponentName,
+  secondLoopComponentUid,
+}) {
+  const {
+    contentTypes,
+    isInDevelopmentMode,
+    removeAttribute,
+  } = useDataManager();
+  const ico = ['integer', 'biginteger', 'float', 'decimal'].includes(type)
+    ? 'number'
+    : type;
+
+  let readableType = type;
+  if (['integer', 'biginteger', 'float', 'decimal'].includes(type)) {
+    readableType = 'number';
+  } else if (['string'].includes(type)) {
+    readableType = 'text';
+  }
+
+  const contentTypeFriendlyName = get(
+    contentTypes,
+    [target, 'schema', 'name'],
+    ''
+  );
+  const src = target ? 'relation' : ico;
+
+  const handleClick = () => {
+    if (configurable !== false) {
+      const attrType = nature ? 'relation' : type;
+      let headerDisplayName = mainTypeName;
+
+      if (firstLoopComponentName) {
+        headerDisplayName = firstLoopComponentName;
+      }
+
+      if (secondLoopComponentUid) {
+        headerDisplayName = secondLoopComponentName;
+      }
+
+      onClick(
+        // Tells where the attribute is located in the main modifiedData object : contentType, component or components
+        editTarget,
+        // main data type uid
+        secondLoopComponentUid || firstLoopComponentUid || targetUid,
+        // Name of the attribute
+        name,
+        // Type of the attribute
+        attrType,
+        headerDisplayName,
+        firstLoopComponentUid ? mainTypeName : null,
+        secondLoopComponentName ? firstLoopComponentName : null,
+        secondLoopComponentUid ? firstLoopComponentUid : null
+      );
+    }
+  };
+  let loopNumber;
+
+  if (secondLoopComponentUid && firstLoopComponentUid) {
+    loopNumber = 2;
+  } else if (firstLoopComponentUid) {
+    loopNumber = 1;
+  } else {
+    loopNumber = 0;
+  }
+
+  return (
+    <Wrapper
+      onClick={handleClick}
+      className={[
+        target ? 'relation-row' : '',
+        configurable ? 'clickable' : '',
+      ]}
+      loopNumber={loopNumber}
+    >
+      <td>
+        <AttributeIcon key={src} type={src} />
+        <Curve fill={isFromDynamicZone ? '#AED4FB' : '#f3f4f4'} />
+      </td>
+      <td style={{ fontWeight: 600 }}>
+        <p>{name}</p>
+      </td>
+      <td>
+        {target ? (
+          <div>
+            <FormattedMessage
+              id={`${pluginId}.modelPage.attribute.relationWith`}
+            />
+            &nbsp;
+            <FormattedMessage id={`${pluginId}.from`}>
+              {msg => (
+                <span style={{ fontStyle: 'italic' }}>
+                  <UpperFist content={contentTypeFriendlyName} />
+                  &nbsp;
+                  {plugin && `(${msg}: ${plugin})`}
+                </span>
+              )}
+            </FormattedMessage>
+          </div>
+        ) : (
+          <>
+            <FormattedMessage id={`${pluginId}.attribute.${readableType}`} />
+            &nbsp;
+            {repeatable && (
+              <FormattedMessage id={getTrad('component.repeatable')} />
+            )}
+          </>
+        )}
+      </td>
+      <td className="button-container">
+        {isInDevelopmentMode && (
+          <>
+            {configurable ? (
+              <>
+                <button type="button" onClick={handleClick}>
+                  <FontAwesomeIcon className="link-icon" icon="pencil-alt" />
+                </button>
+                <button
+                  type="button"
+                  onClick={e => {
+                    e.stopPropagation();
+
+                    removeAttribute(
+                      editTarget,
+                      name,
+                      secondLoopComponentUid || firstLoopComponentUid || ''
+                    );
+                  }}
+                >
+                  <FontAwesomeIcon className="link-icon" icon="trash" />
+                </button>
+              </>
+            ) : (
+              <button type="button">
+                <FontAwesomeIcon icon="lock" />
+              </button>
+            )}
+          </>
+        )}
+      </td>
+    </Wrapper>
+  );
+}
+
+ListRow.defaultProps = {
+  configurable: true,
+  firstLoopComponentName: null,
+  firstLoopComponentUid: null,
+  isFromDynamicZone: false,
+  nature: null,
+  onClick: () => {},
+  onClickDelete: () => {},
+  plugin: null,
+  repeatable: false,
+  secondLoopComponentName: null,
+  secondLoopComponentUid: null,
+  target: null,
+  targetUid: null,
+  type: null,
+};
+
+ListRow.propTypes = {
+  configurable: PropTypes.bool,
+  editTarget: PropTypes.string.isRequired,
+  firstLoopComponentName: PropTypes.string,
+  firstLoopComponentUid: PropTypes.string,
+  isFromDynamicZone: PropTypes.bool,
+  mainTypeName: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  nature: PropTypes.string,
+  onClick: PropTypes.func,
+  plugin: PropTypes.string,
+  repeatable: PropTypes.bool,
+  secondLoopComponentName: PropTypes.string,
+  secondLoopComponentUid: PropTypes.string,
+  target: PropTypes.string,
+  targetUid: PropTypes.string,
+  type: PropTypes.string,
+};
+
+export default memo(ListRow);
+export { ListRow };
---
+++ shiplog/dynamo.js
@@ -0,0 +1,50 @@
+const shiplog = document.querySelector(".shiplog");
+
+document.addEventListener("DOMContentLoaded", () => {
+  logs.reverse().map(log => {
+    const logWrapper = document.createElement("div");
+    logWrapper.className = "log-wrapper";
+    logWrapper.id = log.title.replace(/[^a-zA-Z]+/g, "_").toLowerCase();
+    // meta
+    const logDate = document.createElement("sub");
+    logDate.className = "log-date";
+    logDate.innerHTML = log.date;
+    // title
+    const logTitle = document.createElement("h1");
+    logTitle.className = "log-title";
+    logTitle.innerHTML = log.title;
+    // tags
+    const logTags = document.createElement("div");
+    logTags.className = "log-tags";
+    log.tags.map(tag => {
+      logTag = document.createElement("a");
+      logTag.className = "log-tag";
+      logTag.innerHTML = "#" + tag;
+      logTags.appendChild(logTag);
+    });
+    // body
+    const logBody = document.createElement("div");
+    logBody.className = "log-body";
+    logBody.innerHTML = log.body;
+    // read on medium
+    const readOnMediumButton = document.createElement("a");
+    readOnMediumButton.id = "read-on-medium";
+    readOnMediumButton.href = log.medium;
+    readOnMediumButton.innerHTML = `READ ON <i class='fab fa-medium-m'></i> ↯`;
+    // separator
+    const logSeparator = document.createElement("h1");
+    logSeparator.className = "log-separator";
+    logSeparator.innerHTML = "⌰ ⌬ ↯ ⌰ ⌬ ↯";
+
+    // append to wrapper
+    logWrapper.appendChild(logDate);
+    logWrapper.appendChild(logTitle);
+    logTags.prepend(readOnMediumButton);
+    logWrapper.appendChild(logTags);
+    logWrapper.appendChild(logBody);
+    logWrapper.appendChild(logSeparator);
+
+    // append to shiplog
+    shiplog.appendChild(logWrapper);
+  });
+});
