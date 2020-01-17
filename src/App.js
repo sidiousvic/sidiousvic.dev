1c8a2dcdf9c1995dc343d3b35bf0a95b794aeabe
sidiousvic <victor6rene@gmail.com>
2020-01-17 23:23:05 +0900

    setting up webpack/react

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
 import ReactDOM from "react-dom";
+import React from "react";
+import "./App.css";
 
-ReactDOM.render(<h1>Hello, world!</h1>, document.getElementById("root"));
+ReactDOM.render(<h1>JUST DO SHIT.</h1>, document.getElementById("root"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,29 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
 module.exports = {
+  entry: {
+    app: "./src/App.js"
+  },
+  mode: "development",
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist",
+    hot: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "@SIDIOUSVIC",
+      template: "./src/index.html",
+      filename: "./index.html"
+    }),
+    new CleanWebpackPlugin()
+  ],
   module: {
     rules: [
       {
@@ -7,6 +32,26 @@ module.exports = {
         use: {
           loader: "babel-loader"
         }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ["file-loader"]
+      },
+      {
+        test: /\.(csv|tsv)$/,
+        use: ["csv-loader"]
+      },
+      {
+        test: /\.xml$/,
+        use: ["xml-loader"]
       }
     ]
   }
