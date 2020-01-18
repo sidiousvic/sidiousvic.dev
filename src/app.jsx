5ced3cbdd4252842ab207d3650425c90ac493f70
sidiousvic <victor6rene@gmail.com>
2020-01-18 16:51:03 +0900

    Renamed components

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,5 @@
 import ReactDOM from "react-dom";
 import React from "react";
-import MOTHERBOARD from "./components/Motherboard";
+import MOTHERBOARD from "./components/Motherboard/Motherboard.jsx";
 
 ReactDOM.render(<MOTHERBOARD />, document.getElementById("root"));
---
+++ src/components/Motherboard/Motherboard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ThreeScene from "../ThreeScene/ThreeScene.jsx";
+import "./styles.css";
+
+export default function MOTHERBOARD() {
+  return (
+    <div>
+      {/* REACT THREE FIBER */}
+      <ThreeScene />
+      {/* REACT THREE FIBER */}
+    </div>
+  );
+}
