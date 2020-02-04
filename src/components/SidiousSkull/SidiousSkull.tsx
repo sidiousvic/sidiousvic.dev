bb98baee67a55e0be71888d2d69ff76dcc3e543f
sidiousvic <victor6rene@gmail.com>
2020-02-05 00:37:04 +0900

    refactored SidiousSkull w proper ts types

--- src/components/SidiousSkull/SidiousSkull.tsx
+++ src/components/SidiousSkull/SidiousSkull.tsx
@@ -1,63 +1,57 @@
-import React, { useEffect, useRef } from "react";
-// @ts-ignore( TS2307: Cannot find module '../../assets/models/sidiousSkull.obj'.)
-import sidiousSkull from "../../assets/models/sidiousSkull.obj";
+import React, { useEffect } from "react";
 import { MeshToonMaterial, Color, OBJLoader } from "../../three.exports";
-import { useFrame, useLoader } from "react-three-fiber";
+import { useFrame, useLoader, useUpdate } from "react-three-fiber";
+import { SidiousSkullProps } from "./types";
+import SidiousSkullModel from "../../assets/models/SidiousSkull.obj";
 
-const SidiousSkull = (props: any) => {
-  const eyeL = useRef();
-  const eyeR = useRef();
+const SidiousSkull: React.FC<SidiousSkullProps> = props => {
+  const eyeL = useUpdate((mesh: THREE.Mesh) => {
+    mesh.position.set(-0.24, 0.19, 0.55);
+  }, []);
+  const eyeR = useUpdate((mesh: THREE.Mesh) => {
+    mesh.position.set(0.24, 0.19, 0.55);
+  }, []);
 
   useFrame(({ camera, scene }) => {
     camera.position.y = (props.mouse.y - camera.position.z) * 0.06;
     camera.position.x = (-props.mouse.x - camera.position.z) * 0.04;
     camera.lookAt(scene.position);
-
-    //TODO: FIGURE OUT HOW TO AVOID RESET OF EYES WHEN MOVING
-    // // @ts-ignore( TS2339: Property 'position' does not exist on type 'MutableRefObject<undefined>')
-    // eyeL.current.position.y += Math.cos(clock.getElapsedTime() * 2) * 0.0005;
-    // // @ts-ignore( TS2339: Property 'position' does not exist on type 'MutableRefObject<undefined>')
-    // eyeL.current.position.x += Math.cos(clock.getElapsedTime() * 2) * 0.0003;
-    // // @ts-ignore( TS2339: Property 'position' does not exist on type 'MutableRefObject<undefined>')
-    // eyeR.current.position.y += Math.cos(clock.getElapsedTime() * 2) * 0.0005;
-    // // @ts-ignore( TS2339: Property 'position' does not exist on type 'MutableRefObject<undefined>')
-    // eyeR.current.position.x -= Math.cos(clock.getElapsedTime() * 2) * 0.0003;
   });
 
-  const obj = useLoader(OBJLoader, sidiousSkull);
+  useFrame(({ clock }) => {
+    if (eyeL.current && eyeR.current) {
+      eyeL.current.position.y += Math.cos(clock.getElapsedTime() * 2) * 0.0005;
+      eyeL.current.position.x += Math.sin(clock.getElapsedTime() * 2) * 0.0003;
+      eyeR.current.position.y += Math.cos(clock.getElapsedTime() * 2) * 0.0005;
+      eyeR.current.position.x -= Math.sin(clock.getElapsedTime() * 2) * 0.0003;
+    }
+  });
+
+  const obj = useLoader(OBJLoader, SidiousSkullModel);
   useEffect(() => {
     obj.children.map(child => {
       if (child.type === "Mesh") {
-        //@ts-ignore(TS2339: Property 'material' does not exist on type 'Object3D'.)
-        child.material = new MeshToonMaterial({ color: new Color(0x000000) });
+        (child as THREE.Mesh).material = new MeshToonMaterial({
+          color: new Color(0x000000)
+        });
       }
     });
   }, []);
 
   return (
-    <primitive onClick={() => {}} object={obj}>
+    <primitive object={obj}>
       {/* l eye */}
-      <pointLight color={new Color(0xff0020)}>
-        <mesh
-          ref={eyeL}
-          visible
-          position={[-0.24, 0.18, 0.55]}
-          rotation={[0, 0, 0]}
-        >
+      <pointLight color={new Color(0xff0050)} intensity={2} distance={50}>
+        <mesh ref={eyeL} visible>
           <sphereGeometry attach="geometry" args={[0.02, 20, 20]} />
-          <meshToonMaterial attach="material" color={0xff0044} />
+          <meshToonMaterial attach="material" color={0xff0020} />
         </mesh>
       </pointLight>
       {/* r eye */}
-      <pointLight color={new Color(0xff0020)}>
-        <mesh
-          ref={eyeR}
-          visible
-          position={[0.24, 0.18, 0.55]}
-          rotation={[0, 0, 0]}
-        >
+      <pointLight color={new Color(0xff0050)} intensity={2} distance={50}>
+        <mesh ref={eyeR} visible>
           <sphereGeometry attach="geometry" args={[0.02, 20, 20]} />
-          <meshToonMaterial attach="material" color={0xff0044} />
+          <meshToonMaterial attach="material" color={0xff0020} />
         </mesh>
       </pointLight>
     </primitive>
