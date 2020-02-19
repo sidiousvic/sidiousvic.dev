7aa06564321e0847b2c3fce8d0e19ff55e77a22b
sidiousvic <victor6rene@gmail.com>
2020-02-20 00:34:39 +0900

    update navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,12 @@
 import React, { ReactElement } from "react";
 import Navi from "./Navi";
 // eslint-disable-next-line import/no-webpack-loader-syntax
-import ThunderLogo from "!svg-react-loader!../images/thunderLogo.svg";
+// import ThunderLogo from "!svg-react-loader!../images/thunderLogo.svg";
 
 const Navbar = (): ReactElement => {
   return (
     <nav>
-      <ThunderLogo width={30} />
+      {/* <ThunderLogo width={30} /> */}
       <Navi value="SIDIOUSVIC.DEV" />
       <Navi value="UNDER DEVELOPMENT" />
       {/* <Navi value="Music" />
