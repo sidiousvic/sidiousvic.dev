48abd74a0ca951161b41693fde32bda5d4f648bf
sidiousvic <victor6rene@gmail.com>
2020-02-26 23:23:54 +0900

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,19 +1,31 @@
 import React, { ReactElement } from "react";
-import Navi from "./Navi";
 // eslint-disable-next-line import/no-webpack-loader-syntax
 // import ThunderLogo from "!svg-react-loader!../images/thunderLogo.svg";
+import Navis from "../components/Navis";
+import Logotype from "../components/Logotype";
+
+import styled from "@emotion/styled";
+
+const StyledNav = styled.nav`
+  position: fixed;
+  display: flex;
+  justify-content: space-between;
+  padding: 0 4rem;
+  align-items: center;
+  background-color: transparent;
+  z-index: 100;
+  width: 100%;
+  height: 10%;
+`;
 
 const Navbar = (): ReactElement => {
   return (
-    <nav>
-      {/* <ThunderLogo width={30} /> */}
-      <Navi value="SIDIOUSVIC.DEV" />
-      <Navi value="UNDER DEVELOPMENT" />
-      {/* <Navi value="Music" />
-      <Navi value="Vicelog" />
-      <Navi value="Github" />
-      <Navi value="Instagram" /> */}
-    </nav>
+    <StyledNav>
+      <Logotype value="vic sidious" />
+      {/* <ThunderLogo width={60} /> */}
+
+      <Navis />
+    </StyledNav>
   );
 };
 
