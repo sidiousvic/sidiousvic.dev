b90579bd2046c3a8fc236621e0cbd0ba86e46646
sidiousvic <victor6rene@gmail.com>
2020-02-26 23:26:33 +0900

    empty shell of a component

--- src/pages/vicelog.tsx
+++ src/pages/vicelog.tsx
@@ -1,23 +1,7 @@
 import React from "react";
-import { Link } from "gatsby";
 
 const Vicelog: React.FC = () => {
-  return (
-    <div>
-      <Link
-        to="/"
-        style={{
-          color: "lime",
-          position: "absolute",
-          top: "50%",
-          right: "25%",
-          zIndex: 99
-        }}
-      >
-        BACK
-      </Link>
-    </div>
-  );
+  return <></>;
 };
 
 export default Vicelog;
