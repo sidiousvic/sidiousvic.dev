383825d1688e66797d6f9af22a2623a98a3b6f7f
sidiousvic <victor6rene@gmail.com>
2020-02-26 23:25:36 +0900

    style title

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,13 +1,31 @@
 import React, { ReactElement } from "react";
+import styled from "@emotion/styled";
 import useZ from "../zustand/z";
+
+const StyledTitle = styled.div`
+  font-size: 3.5vw;
+  pointer-events: none;
+  user-select: none;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-family: Arial, Helvetica, sans-serif;
+  color: var(--sidious-red);
+  mix-blend-mode: difference;
+  z-index: 99;
+`;
 
 const Title = (): ReactElement => {
   const title = useZ(z => z.title);
 
   return (
-    <div className="title">
+    <StyledTitle>
       <h1>{title}</h1>
-    </div>
+    </StyledTitle>
   );
 };
 
