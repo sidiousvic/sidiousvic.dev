7d4cad621008ca2f752632b154791df55f03962e
sidiousvic <victor6rene@gmail.com>
2020-02-26 23:27:00 +0900

    updated title

--- src/zustand/z.ts
+++ src/zustand/z.ts
@@ -2,7 +2,7 @@ import create from "zustand";
 
 const [useZ] = create(set => ({
   eyeVelocity: 0,
-  title: "@SIDIOUSVIC",
+  title: "JUST DO SH*T.",
   setTitle: (title: string): void => {
     set({ title });
   },
