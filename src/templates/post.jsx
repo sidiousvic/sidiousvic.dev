4cde2d3ea483634d3e77f19a2b5a7297c542a6b7
sidiousvic <victor6rene@gmail.com>
2020-03-05 22:34:13 +0900

    remove emotion

--- src/templates/post.jsx
+++ src/templates/post.jsx
@@ -1,8 +1,5 @@
 import React from "react";
 import { graphql } from "gatsby";
-// import styled from "@emotion/styled";
-
-// const styled
 
 const Post = ({ data: { prismicPost } }) => {
   const { data } = prismicPost;
