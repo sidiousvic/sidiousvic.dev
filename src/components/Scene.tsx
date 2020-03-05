e3772cb9b83df324166d8a1c5b741c941e9f8492
sidiousvic <victor6rene@gmail.com>
2020-03-05 22:33:04 +0900

    remove superflous skull logic

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -6,16 +6,8 @@ import { useWindowSize } from "../hooks";
 import { window } from "browser-monads";
 
 const Scene: React.FC = () => {
-  const setEyeVelocity = useZ(z => z.setEyeVelocity);
   const setMouse = useZ(z => z.setMouse);
-  const title = useZ(z => z.title);
-  const setTitle = useZ(z => z.setTitle);
   const windowSize = useWindowSize(900);
-
-  const toggleTitle = (): void => {
-    if (title === "VIC SIDIOUS") setTitle("JUST DO SH*T.");
-    if (title === "JUST DO SH*T.") setTitle("VIC SIDIOUS");
-  };
 
   window.addEventListener("mousemove", (e: MouseEvent) => {
     setMouse({
@@ -26,11 +18,8 @@ const Scene: React.FC = () => {
 
   return (
     <Canvas
-      onClick={(): void => {
-        toggleTitle();
-        setEyeVelocity(0.03);
-      }}
-      camera={{ fov: 35, near: 0.1, far: 100, position: [0, 0, 7] }}
+      invalidateFrameloop
+      camera={{ fov: 35, near: 0.1, far: 100, position: [0, 0, 4.5] }}
       pixelRatio={window.devicePixelRatio}
     >
       <Suspense fallback={null}>
