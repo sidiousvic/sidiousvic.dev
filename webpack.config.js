8e46d3d2e3e243f85e713a3faa569093cb9d74ea
sidiousvic <victor6rene@gmail.com>
2020-05-08 08:13:56 +0900

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,84 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+// const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+
+module.exports = {
+  entry: {
+    app: "./src/index.tsx"
+  },
+  resolve: {
+    alias: {
+      // Forward all three imports to our exports file
+      three$: path.resolve("./src/three.x.ts")
+    },
+    extensions: [".tsx", ".ts", ".js"]
+  },
+  mode: "development",
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist"
+    // hot: true
+  },
+  optimization: {
+    minimizer: [new UglifyJsPlugin()]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "🔥GENERIC🌻DARKLORD🍕VIC👽SIDIOUS🔮",
+      filename: "./index.html",
+      template: "src/index.html"
+    }),
+    new CleanWebpackPlugin()
+    // new BundleAnalyzerPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(png|jpg|gif|ico)$/,
+        use: ["file-loader"]
+      },
+      {
+        test: /\.(svg)$/,
+        use: ["svg-react-loader"]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ["file-loader"]
+      },
+      {
+        test: /\.obj$/,
+        use: ["file-loader"]
+      },
+      {
+        test: /\.(csv|tsv)$/,
+        use: ["csv-loader"]
+      },
+      {
+        test: /\.xml$/,
+        use: ["xml-loader"]
+      }
+    ]
+  }
+};
