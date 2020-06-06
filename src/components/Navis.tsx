ef6351980f7f01ec80ad68516792690c37d54bf3
sidiousvic <victor6rene@gmail.com>
2020-06-06 20:46:53 +0900

    Added BLM donation link

--- src/components/Navis.tsx
+++ src/components/Navis.tsx
@@ -1,18 +1,37 @@
 import React from "react";
 import styled from "@emotion/styled";
-import Navi from "../components/Navi";
+import BlmSvg from "../images/blm.svg";
 
 const StyledNavis = styled.div`
   display: flex;
   flex-direction: row;
+  mix-blend-mode: difference;
 `;
 
-const Navis: React.FC = () => {
+function Navis() {
   return (
     <StyledNavis>
-      <Navi value="DON'T WANNA DIE WITHOUT A FEW SCARS" />
+      <a
+        href="https://secure.actblue.com/donate/ms_blm_homepage_2019"
+        target="_blank"
+      >
+        <BlmSvg width={"80px"} />
+        {/* <div
+          style={{
+            background: `url(${blmUrl})`,
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+            // width: "60px",
+            height: "100%",
+            top: "-3px",
+            // border: "black 1px solid",
+            mixBlendMode: "difference",
+            cursor: "pointer"
+          }}
+        ></div> */}
+      </a>
     </StyledNavis>
   );
-};
+}
 
 export default Navis;
