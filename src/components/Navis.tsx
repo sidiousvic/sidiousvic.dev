47a2d7a034acc60e8b14745bae4b8b3f293ac705
sidiousvic <victor6rene@gmail.com>
2020-06-08 20:46:48 +0900

    Added BLM logos

--- src/components/Navis.tsx
+++ src/components/Navis.tsx
@@ -1,20 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "@emotion/styled";
 import BlmSvg from "../images/blm.svg";
+import BlmDonateSvg from "../images/blmDonate.svg";
 
 const StyledNavis = styled.div`
   display: flex;
   flex-direction: row;
+  border-radius: 30%;
 `;
 
 function Navis() {
+  const [blmSvgDonate, setBlmSvgDonate] = useState(false);
+
+  function toggleBlmDonate() {
+    setBlmSvgDonate(!blmSvgDonate);
+  }
+
   return (
     <StyledNavis>
       <a
+        onMouseEnter={() => {
+          toggleBlmDonate();
+        }}
+        onMouseLeave={() => {
+          toggleBlmDonate();
+        }}
         href="https://secure.actblue.com/donate/ms_blm_homepage_2019"
         target="_blank"
       >
-        <BlmSvg className="blm-svg" width={"80px"} />
+        {blmSvgDonate ? (
+          <BlmDonateSvg className="blm-svg" width={"80px"} />
+        ) : (
+          <BlmSvg className="blm-svg" width={"80px"} />
+        )}
       </a>
     </StyledNavis>
   );
