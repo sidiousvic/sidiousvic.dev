03afdb7e67a841666da9b0ce4c17b414781b7ea5
sidiousvic <victor6rene@gmail.com>
2020-06-14 14:56:22 +0900

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom";
 import Scene from "./components/Scene";
 import Title from "./components/Title";
+import About from "./components/About";
 import Navbar from "./components/Navbar";
 import BornAndRaised from "./components/BornAndRaised";
 import "./styles.css";
@@ -9,8 +10,10 @@ import { Global, css } from "@emotion/core";
 import Nikes from "./fonts/FuturaCondensedExtraBoldOblique.otf";
 import Flag from "./fonts/Flag.ttf";
 import NeueMachina from "./fonts/NeueMachina-Regular.otf";
-
 import NeueMachinaUltrabold from "./fonts/NeueMachina-Ultrabold.otf";
+
+const CLIENT_ID = "dbc4fa45ace94914bd97b4b6e28388f2";
+const CLIENT_SECRET = "2faa83dc0acd4d80994533068bf5b0fd";
 
 const styles = css`
   @font-face {
@@ -32,10 +35,23 @@ const styles = css`
 `;
 
 function MOTHERBOARD() {
+  useEffect(() => {
+    fetch("https://accounts.spotify.com/api/token", {
+      method: "post",
+      body: JSON.stringify({ grant_type: "client_credentials" }),
+      headers: {
+        Authorization:
+          "Basic " +
+          new Buffer(CLIENT_ID + ":" + CLIENT_SECRET).toString("base64")
+      }
+    }).then(s => console.log(s));
+  }, []);
+
   return (
     <>
       <Global styles={styles} />
       <Navbar />
+      <About />
       <Title />
       <Scene />
       <div
@@ -48,6 +64,9 @@ function MOTHERBOARD() {
         }}
       >
         <BornAndRaised />
+        <div className="spotify">
+          <h1 style={{ fontFamily: "Flag" }}>TODAY'S MENU</h1>
+        </div>
       </div>
     </>
   );
