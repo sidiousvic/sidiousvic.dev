365a17d2ee122c4c656c373d53c9ede1c77b1256
sidiousvic <victor6rene@gmail.com>
2020-06-14 14:56:30 +0900

    Update z.ts

--- src/zustand/z.ts
+++ src/zustand/z.ts
@@ -1,16 +1,18 @@
 import create from "zustand";
 
 const [useZ] = create(set => ({
-  eyeVelocity: 0,
   title: "Vic Sidious",
   setTitle: (title: string): void => {
     set({ title });
   },
+  eyeVelocity: 0,
   setEyeVelocity: (eyeVelocity: number): void => {
     set({ eyeVelocity });
   },
   mouse: { x: 0, y: 0 },
-  setMouse: (mouse: number): void => set({ mouse })
+  setMouse: (mouse: number): void => set({ mouse }),
+  about: false,
+  setAbout: (about: boolean): void => set({ about })
 }));
 
 export default useZ;
