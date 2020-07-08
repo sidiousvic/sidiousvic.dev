2876749b8da41008278ae222eafc0e9c55afa53b
sidiousvic <victor6rene@gmail.com>
2020-07-08 21:51:32 +0900

    using phantom

--- src/MOTHERBOARD.ts
+++ src/MOTHERBOARD.ts
@@ -1,61 +1,66 @@
 import "./styles/global.css";
-import PHANTOM from "./PHANTOM";
 import SidiousSkull from "./three/SidiousSkull";
+import reduxStore from "./redux";
 import Title from "./ui/Title";
 import BornAndRaised from "./ui/BornAndRaised";
-export const { FIRE, WATCH, DATA, LAUNCH } = PHANTOM(XDOM);
+import phantom from "@sidiousvic/phantom";
+// import phantom from "./PHANTOM";
+export const { fire, data, launch } = phantom(reduxStore, XDOM);
+// phantom
+launch();
 
-LAUNCH();
-
-SidiousSkull();
-
-function XDOM() {
+function XDOM(): any {
+  const { pizza, desertXicuahua } = data();
   return `
-  ${Title()}
-  ${BornAndRaised()}
+  ${Title(pizza)}
+  ${BornAndRaised(desertXicuahua)}
   `;
 }
 
-WATCH("click", justDoShit);
+// plugins
+SidiousSkull(); // three scene
+
+// listeners
+document.addEventListener("click", justDoShit);
 function justDoShit(e: MouseEvent) {
-  if ((e.target as HTMLDivElement).classList.contains("title-h1")) {
-    FIRE({ type: "JUST_DO_SHIT" });
+  if ((e.target as HTMLDivElement).id === "title-h1") {
+    fire({ type: "JUST_DO_SHIT" });
     moveBornAndRaised(e);
   }
 }
 
-WATCH("mousedown", desertXicuahua);
-WATCH("mouseup", desertXicuahua);
+document.addEventListener("mousedown", desertXicuahua);
+document.addEventListener("mouseup", desertXicuahua);
 function desertXicuahua(e: MouseEvent) {
-  if ((e.target as HTMLParagraphElement).classList.contains("desert-p")) {
-    FIRE({ type: "DESERT_XICUAHUA" });
+  if ((e.target as HTMLParagraphElement).id === "desert-p") {
+    fire({ type: "DESERT_XICUAHUA" });
     moveBornAndRaised(e);
   }
 }
 
-WATCH("mousemove", moveBornAndRaised);
+document.addEventListener("mousemove", moveBornAndRaised);
 function moveBornAndRaised(e: MouseEvent) {
   const mouse = {
-    x: (e.clientX - window.innerWidth / 2) / 2,
-    y: (e.clientY - window.innerHeight / 2) / 2
+    x: e.clientX / window.innerWidth,
+    y: e.clientY / window.innerHeight
   };
 
   const xicuahuaImg: HTMLImageElement | null = document.querySelector(
-    ".xicuahua-img"
+    "#xicuahua-img"
   );
   const desertP: HTMLParagraphElement | null = document.querySelector(
-    ".desert-p"
+    "#desert-p"
   );
   const BornAndRaisedP: HTMLParagraphElement | null = document.querySelector(
-    ".born-and-raised-p"
+    "#born-and-raised-p"
   );
 
-  xicuahuaImg!.style.transform = `translate(${mouse.x * 0.01}px, ${mouse.y *
-    0.01}px)`;
+  xicuahuaImg!.style.transform = `translate(${mouse.x * 14}px, ${mouse.y *
+    14}px)`;
 
-  desertP!.style.transform = `translate(${mouse.y * 0.04}%, ${mouse.x *
-    0.04}%) skew(${mouse.y * 0.02}deg, ${mouse.x * 0.02}deg)`;
+  desertP!.style.transform = `translate(${mouse.y * 6}%, ${mouse.x *
+    6}%) skew(${mouse.y * 7}deg, ${mouse.x * 7}deg)`;
 
-  BornAndRaisedP!.style.transform = `translate(${mouse.x * 0.03}%, ${mouse.y *
-    0.03}%)`;
+  BornAndRaisedP!.style.transform = `translate(${mouse.x * 8}%, ${mouse.y *
+    8}%)`;
 }
