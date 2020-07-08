3cf8d2dc4442f76bc01e2a20b350a44bb0a942af
sidiousvic <victor6rene@gmail.com>
2020-07-08 21:52:47 +0900

    update BornAndRaised.ts

--- src/ui/BornAndRaised.ts
+++ src/ui/BornAndRaised.ts
@@ -1,16 +1,13 @@
 import xicuahuaUrl from "../assets/images/xicuahua.svg";
-import { DATA } from "../MOTHERBOARD";
 
-export default function BornAndRaised() {
-  const { desertXicuahua } = DATA();
-
-  const cursor = desertXicuahua === "DESERT" ? "grab" : "grabbing";
+export default function BornAndRaised(desertXicuahua: string) {
+  const cursorStyle = desertXicuahua === "DESERT" ? "grab" : "grabbing";
   return `
-  <div class="born-and-raised-div">
-    <div class="scorpion-div">
-      <p class="born-and-raised-p">BORN AND RAISED IN THE</p>
-      <p class="desert-p" style="cursor: ${cursor}">${desertXicuahua}</p>
-      <img class="xicuahua-img" src="${xicuahuaUrl}" alt="Outline of Chihuahuan border, Mexico">
+  <div data-phantom=${desertXicuahua} id="born-and-raised-div">
+    <div id="scorpion-div">
+      <p id="born-and-raised-p">BORN AND RAISED IN THE</p>
+      <p id="desert-p" style="cursor: ${cursorStyle}">${desertXicuahua}</p>
+      <img id="xicuahua-img" src=${xicuahuaUrl} alt="Outline of Chihuahuan border, Mexico">
     </div>
   </div>
     `;
