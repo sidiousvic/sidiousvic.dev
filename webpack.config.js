ed4d70630da2b19f09b03d34cfa319139107ed46
sidiousvic <victor6rene@gmail.com>
2020-07-12 06:34:13 +0900

    copy update

--- webpack.config.js
+++ webpack.config.js
@@ -7,81 +7,81 @@ const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: {
-    app: "./src/MOTHERBOARD.ts"
+    app: path.resolve("./src/MOTHERBOARD.ts"),
   },
   resolve: {
     alias: {
       // Forward all three imports to our exports file
-      three$: path.resolve("./src/three.x.ts")
+      three$: path.resolve("./src/three.x.ts"),
     },
-    extensions: [".ts", ".js"]
+    extensions: [".ts", ".js"],
   },
   mode: "development",
   output: {
     filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist")
+    path: path.resolve(__dirname, "dist"),
   },
   devtool: "inline-source-map",
   devServer: {
-    contentBase: "./dist"
+    contentBase: "./dist",
     // hot: true
   },
   optimization: {
-    minimizer: [new UglifyJsPlugin()]
+    minimizer: [new UglifyJsPlugin()],
   },
   plugins: [
     new HtmlWebpackPlugin({
       filename: "./index.html",
-      template: "src/index.html"
+      template: "src/index.html",
     }),
     new CleanWebpackPlugin(),
     // new BundleAnalyzerPlugin(),
     new CopyPlugin({
-      patterns: [{ from: "static", to: "static" }]
-    })
+      patterns: [{ from: "static", to: "static" }],
+    }),
   ],
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
+        test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
-        }
+          loader: "babel-loader",
+        },
       },
       {
         test: /\.tsx?$/,
         loader: "ts-loader",
-        exclude: /node_modules/
+        exclude: /node_modules/,
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"]
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.(png|jpg|gif|ico)$/,
-        use: ["file-loader"]
+        use: ["file-loader"],
       },
       {
         test: /\.(svg)$/,
-        use: ["file-loader"]
+        use: ["file-loader"],
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: ["file-loader"]
+        use: ["file-loader"],
       },
       {
         test: /\.obj$/,
-        use: ["file-loader"]
+        use: ["file-loader"],
       },
       {
         test: /\.(csv|tsv)$/,
-        use: ["csv-loader"]
+        use: ["csv-loader"],
       },
       {
         test: /\.xml$/,
-        use: ["xml-loader"]
-      }
-    ]
-  }
+        use: ["xml-loader"],
+      },
+    ],
+  },
 };
